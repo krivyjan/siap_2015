48f739fd2976a4f471d454a44c8b1452fb9f5f15
angolinha <xsustek.martin@gmail.com>
2015-05-07 15:39:35 +0200

    Zmena .class suboru za .jar subor, ktory bude pustany z CMD

--- TimeStampFactory.java
+++ TimeStampFactory.java
@@ -3,10 +3,13 @@ import org.bouncycastle.util.encoders.Base64;
 
 public class TimeStampFactory {
 
-    public static String getTimeStamp(byte[] data) throws Exception {
+    public static void main(String[] args) throws Exception {
+        String data = args[0];
         TimeStampResponse response = new TimeStampResponse(Base64.decode(data));
         String encResponse = new String(Base64.encode(response.getTimeStampToken().getEncoded()));
 
-        return encResponse;
+        System.out.println(encResponse);
+
+        return;
     }
 }
